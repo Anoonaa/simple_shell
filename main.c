f69d31c95c635169e8d277654a43ec2b8614216f
Anoona Sithole <anoonasithole0@gmail.com>
2024-06-22 20:10:30 +0200

    extracted error handling + defined constants for magic numbers

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include "shell.h"
 
+#define STDERR_FILENO 2
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
 /**
  * main - entry point
  * @ac: arg count
@@ -9,36 +13,52 @@
  */
 int main(int ac, char **av)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+    info_t info = INFO_INIT;
+    int fd = STDERR_FILENO;
 
-	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (fd)
-			: "r" (fd));
+    // Why the inline assembly? Can we use a simple addition instead?
+    fd += 3;
 
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+    if (ac == 2) {
+        fd = open(av[1], O_RDONLY);
+        if (fd == -1) {
+            handle_open_error(av[0], av[1], errno);
+            return EXIT_FAILURE;
+        }
+        info.readfd = fd;
+    }
+
+    populate_env_list(&info);
+    read_history(&info);
+    hsh(&info, av);
+    return EXIT_SUCCESS;
+}
+
+/**
+ * handle_open_error - handle errors from open function
+ * @prog_name: program name
+ * @filename: file name
+ * @errno: error number
+ */
+void handle_open_error(const char *prog_name, const char *filename, int errno)
+{
+    switch (errno) {
+        case EACCES:
+            exit(126);
+        case ENOENT:
+            _eputs(prog_name);
+            _eputs(": 0: Can't open ");
+            _eputs(filename);
+            _eputchar('\n');
+            _eputchar(BUF_FLUSH);
+            exit(127);
+        default:
+            // Handle other errors here
+            _eputs(prog_name);
+            _eputs(": Error opening file ");
+            _eputs(filename);
+            _eputchar('\n');
+            _eputchar(BUF_FLUSH);
+            exit(EXIT_FAILURE);
+    }
 }
