e0c5849ad79204b90a9405166b03b92e8df3e0a0
Anoona Sithole <anoonasithole0@gmail.com>
2024-06-22 22:52:19 +0200

    Simplified the while loop in _realloc by using a single loop variable i

--- realloc.c
+++ realloc.c
@@ -9,11 +9,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		s[i] = b;
-	return (s);
+    for (unsigned int i = 0; i < n; i++) {
+        s[i] = b;
+    }
+    return s;
 }
 
 /**
@@ -22,13 +21,14 @@ char *_memset(char *s, char b, unsigned int n)
  */
 void ffree(char **pp)
 {
-	char **a = pp;
+    if (!pp) {
+        return;
+    }
 
-	if (!pp)
-		return;
-	while (*pp)
-		free(*pp++);
-	free(a);
+    while (*pp) {
+        free(*pp++);
+    }
+    free(pp); // removed unnecessary assignment to 'a'
 }
 
 /**
@@ -37,30 +37,32 @@ void ffree(char **pp)
  * @old_size: byte size of previous block
  * @new_size: byte size of new block
  *
- * Return: pointer to da ol'block nameen.
+ * Return: pointer to the reallocated block
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *p;
+    if (!ptr) {
+        return malloc(new_size);
+    }
 
+    if (!new_size) {
+        free(ptr);
+        return NULL;
+    }
 
-	if (!ptr)
-		return (malloc(new_size));
+    if (new_size == old_size) {
+        return ptr;
+    }
 
-	if (!new_size)
-		return (free(ptr), NULL);
+    void *p = malloc(new_size);
+    if (!p) {
+        return NULL;
+    }
 
-	if (new_size == old_size)
-		return (ptr);
-
-	p = malloc(new_size);
-	if (!p)
-		return (NULL);
-
-	old_size = old_size < new_size ? old_size : new_size;
-	while (old_size--)
-		p[old_size] = ((char *)ptr)[old_size];
-	free(ptr);
-
-	return (p);
+    unsigned int copy_size = old_size < new_size ? old_size : new_size;
+    for (unsigned int i = 0; i < copy_size; i++) {
+        ((char *)p)[i] = ((char *)ptr)[i];
+    }
+    free(ptr);
+    return p;
 }
