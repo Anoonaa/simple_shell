1c9b720cfa33811a78377233af684da4b73ebabb
Anoona Sithole <anoonasithole0@gmail.com>
2024-06-22 22:42:41 +0200

    fixed '_tract' function

--- string.c
+++ string.c
@@ -8,37 +8,39 @@
  */
 int _strlen(char *s)
 {
-	int i = 0;
+    int len = 0;
 
-	if (!s)
-		return (0);
+    if (!s)
+        return (0);
 
-	while (*s++)
-		i++;
+    while (*s++)
+        len++;
 
-	return (i);
+    return (len);
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
+ * _strcmp - performs lexicographic comparison of two strings
+ * @s1: the first string
+ * @s2: the second string
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2)
-	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
-		s1++;
-		s2++;
-	}
-	if (*s1 == *s2)
-		return (0);
-	else
-		return (*s1 < *s2 ? -1 : 1);
+    while (*s1 && *s2)
+    {
+        if (*s1 != *s2)
+            return (*s1 - *s2);
+        s1++;
+        s2++;
+    }
+    if (*s1 == *s2)
+        return (0);
+    else if (*s1 < *s2)
+        return (-1);
+    else
+        return (1);
 }
 
 /**
@@ -50,11 +52,12 @@ int _strcmp(char *s1, char *s2)
  */
 char *starts_with(const char *haystack, const char *needle)
 {
-	while (*needle)
-		if (*needle++ != *haystack++)
-			return (NULL);
-
-	return ((char *)haystack);
+    while (*needle)
+    {
+        if (*needle++ != *haystack++)
+            return (NULL);
+    }
+    return ((char *)haystack - (needle - 1));
 }
 
 /**
@@ -66,16 +69,15 @@ char *starts_with(const char *haystack, const char *needle)
  */
 char *_strcat(char *dest, char *src)
 {
-	char *ret = dest;
+    char *ret = dest;
 
+    while (*dest)
+        dest++;
 
-	while (*dest)
-		dest++;
+    while (*src)
+        *dest++ = *src++;
 
-	while (*src)
-		*dest++ = *src++;
+    *dest = '\0';
 
-	*dest = *src;
-
-	return (ret);
+    return (ret);
 }
